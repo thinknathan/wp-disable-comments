9546098f469881ea290383fcc41b4a83836d31ba
Nathan Bolton <nathan@nathanbolton.ca>
2017-08-31 00:19:03 -0700

    Use namespace & removed pingback functions

--- wp-disable-comments.php
+++ wp-disable-comments.php
@@ -3,35 +3,37 @@
 Plugin Name:  Disable Comments
 Plugin URI:   https://github.com/thinknathan/
 Description:  Disable all comments.
-Version:      1.0.0
+Version:      1.0.1
 Author:       Think_Nathan
 Author URI:   https://thinknathan.ca
 License:      MIT License
 */
 
+namespace Think_Nathan\Utils;
+
 // Credit to https://wordpress.org/plugins/disable-comments/
 // which is a more comprehensive, better version of this plugin
 
 // Remove comments page and comments options page
-function n_filter_admin_menu(){
+function filter_admin_menu() {
   remove_menu_page( 'edit-comments.php' );
   remove_submenu_page( 'options-general.php', 'options-discussion.php' );
 }
-add_action( 'admin_menu', 'n_filter_admin_menu', 999 );
+add_action( 'admin_menu', __NAMESPACE__ . '\\filter_admin_menu', 999 );
 
 // Remove comments widget in dashboard
-function n_filter_dashboard(){
+function filter_dashboard() {
   remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
 }
-add_action( 'wp_dashboard_setup', 'n_filter_dashboard' );
+add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\\filter_dashboard' );
 
 // Remove comments widget in sidebar
-function n_disable_rc_widget() {
+function disable_rc_widget() {
   unregister_widget( 'WP_Widget_Recent_Comments' );
 }
-add_action( 'widgets_init', 'n_disable_rc_widget' );
+add_action( 'widgets_init', __NAMESPACE__ . '\\disable_rc_widget' );
 
-function n_disable_comments_setup() {
+function disable_comments_setup() {
   // Set comments to closed in options
   update_option( 'default_comment_status', 'closed' );
 
@@ -41,11 +43,7 @@ function n_disable_comments_setup() {
   // Disable comments
   add_filter( 'comments_open', '__return_false', 10, 2 );
 
-  // Disable pingbacks
-  add_filter( 'pings_open', '__return_false', 10, 2 );
-  add_filter( 'pre_option_default_pingback_flag', '__return_zero' );
-
   // Disables comments feed link
   add_filter( 'feed_links_show_comments_feed', '__return_false' );
 }
-add_action( 'wp_loaded', 'n_disable_comments_setup' );
+add_action( 'wp_loaded', __NAMESPACE__ . '\\disable_comments_setup' );
